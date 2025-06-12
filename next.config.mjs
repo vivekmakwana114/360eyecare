27f32fb1be2099d71a9f1aa78cacaa90703037e1
ShAhilK2 <shahil.kataria@invennico.com>
2025-06-12 15:53:32 +0530

    feat: Hardcoded Reviews

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.360eyecare.ca', '360eyecare.snag-zilla.com'],
+    domains: ['www.360eyecare.ca', '360eyecare.snag-zilla.com', 'lh3.googleusercontent.com'],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080],
     imageSizes: [16, 32, 48, 64, 96],
