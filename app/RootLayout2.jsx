07fbf0099114876c93d65fb5a87a4f337ff070b3
ShAhilK2 <shahil.kataria@invennico.com>
2025-07-28 13:01:29 +0530

    fix : marketing layout

--- app/RootLayout2.jsx
+++ app/RootLayout2.jsx
@@ -8,8 +8,8 @@ import { usePathname } from "next/navigation";
 const RootLayout2 = ({ children }) => {
   const hideLayout =
     usePathname() === "/shop/" ||
-    usePathname() === "/book-eye-consultation-yorkville/" ||
-    usePathname() === "/thank-you/";
+    usePathname() === "/book-eye-consultation-yorkville" ||
+    usePathname() === "/thank-you";
 
   return (
     <>
